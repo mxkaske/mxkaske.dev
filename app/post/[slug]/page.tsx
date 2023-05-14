530b7f70fc441db7ede8e99b70bac90aadc073a4
Maximilian Kaske <maximilian@kaske.org>
2023-05-14 18:53:56 +0200

    fix: url

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -7,6 +7,8 @@ import { Link } from "@/components/mdx/link";
 const URL =
   process.env.VERCEL_ENV === "production"
     ? `https://craft.mxkaske/dev`
+    : process.env.VERCEL_ENV === "preview"
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : process.env.NEXT_PUBLIC_VERCEL_URL;
 
 function formatDate(date: Date) {
