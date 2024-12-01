8d838b0cf4dc89d88ba07b551212e022bee1ee80
Maximilian Kaske <maximilian@kaske.org>
2024-12-01 16:41:43 +0100

    fix: middleware redirect

---
+++ middleware.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const host = request.nextUrl.host;
+
+  if (process.env.NODE_ENV === "development") {
+    return NextResponse.next();
+  }
+
+  if (pathname.startsWith("/craft") && !host.includes("craft")) {
+    const newPathname = pathname.replace("/craft", "");
+    return NextResponse.redirect(
+      new URL(newPathname, "https://craft.mxkaske.dev")
+    );
+  }
+
+  if (pathname.startsWith("/brew") && !host.includes("brew")) {
+    const newPathname = pathname.replace("/brew", "");
+    return NextResponse.redirect(
+      new URL(newPathname, "https://brew.mxkaske.dev")
+    );
+  }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - assets (public assets)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|assets|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
