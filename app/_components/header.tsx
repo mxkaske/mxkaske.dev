8d44f848e1b81f0af3b67bc948137d27d5704664
Maximilian Kaske <maximilian@kaske.org>
2025-07-25 13:46:41 +0200

    fix: header active link

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,49 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { Name } from "./name";
 import { Link } from "@/components/mdx/link";
+import { Fragment, useEffect } from "react";
+import { useState } from "react";
+
+function subdomain() {
+  if (typeof window === "undefined") {
+    return null;
+  }
+
+  const host = window.location.host;
+  const subdomain = host.split(".")[0];
+  return subdomain;
+}
+
+const level = [
+  {
+    name: "Home",
+    href: "https://mxkaske.dev",
+    active: "mxkaske",
+  },
+  {
+    name: "Craft",
+    href: "https://craft.mxkaske.dev",
+    active: "craft",
+  },
+  {
+    name: "Brew",
+    href: "https://brew.mxkaske.dev",
+    active: "brew",
+  },
+];
 
 export function Header({
   className,
   ...props
 }: React.PropsWithChildren<React.HTMLAttributes<HTMLElement>>) {
+  const [active, setActive] = useState<string | null>(null);
+
+  useEffect(() => {
+    setActive(subdomain() ?? null);
+  }, []);
+
   return (
     <header
       className={cn("flex items-center justify-between", className)}
@@ -15,17 +53,20 @@ export function Header({
         <Name />
       </div>
       <div>
-        <Link href="https://mxkaske.dev" internal>
-          Home
-        </Link>
-        <span className="mx-2 text-muted-foreground">·</span>
-        <Link href="https://craft.mxkaske.dev" internal>
-          Craft
-        </Link>
-        <span className="mx-2 text-muted-foreground">·</span>
-        <Link href="https://brew.mxkaske.dev" internal>
-          Brew
-        </Link>
+        {level.map((item, index) => (
+          <Fragment key={item.name}>
+            <Link
+              href={item.href}
+              className={cn(active === item.active && "decoration-foreground")}
+              internal
+            >
+              {item.name}
+            </Link>
+            {index < level.length - 1 && (
+              <span className="mx-2 text-muted-foreground">·</span>
+            )}
+          </Fragment>
+        ))}
       </div>
     </header>
   );
